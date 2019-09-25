c6339fb81ec2fa9df07a13aa90684f531af5fed5
utsavpatel5 <54430022+utsavpatel5@users.noreply.github.com>
2019-09-25 02:13:36 -0400

    new class added as per Dr. gibson suggestion
    patient ,nurse class modified.

---
+++ src/Appointment.java
@@ -0,0 +1,11 @@
+package prob1;
+
+public class Appointment {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	public static void manageAppointments() {
+
+}
--- src/Patient.java
+++ src/Patient.java
@@ -1,43 +1,73 @@
+package prob1;
 import java.util.Set;
 
 public class Patient {
-	
-	 public String patient;
+
 	 
+	 public String id;
+	 public String firstName;
+	 public String lastName;
+	 public String patientId;
+	 
+	 public Patient(String id, String firstname, String lastName, String patientId) {
+		 this.id = id;
+		 this.firstName=firstname;
+		 this.lastName= lastName;
+		 this.patientId=patientId;
+		 
+	 }
+	 
+	 
+	 
+
 	 private Receptionist receptionist;
-	 
+
 	 private Doctor doctor;
-	 
+
 	 private Nurse nurse;
 	 
+	 
+	 private Appointment appointment;
+
 	 OneToOne(mappedBy = "receptionist")
 	 public Receptionist getReceptionist() {
 		 return receptionist;
 	 }
-	 
+
 	 @OneToOne(mappedBy = "patient")
 	 public Doctor getDoctor() {
 		 return doctor;
 	 }
-	 
+
 	 @OneToOne(mappedBy = "nurse")
 	 public Nurse getNurse() {
 		 return nurse;
 	 }
-	
+
+     public Appointment getAppointment() {
+    	 return appointment;
+     }
 	public void bookAppointments() {
 	}
-	
+
+
 	public void cancelorChangeAppointments() {
 	}
-	
+
+
 	public void patientInformation() {
 	}
+	
 	public void payTheMedicalBill() {
 	}
-	
+
+
 	public void getPrescriptions() {
 	}
-	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
 
 }
