5cb82565e6a2bf777fd01f9136011b1faf350206
utsavpatel5 <54430022+utsavpatel5@users.noreply.github.com>
2019-10-24 10:10:16 -0400

    sprint-2 created
    Added classes to sprint 2 folder

---
+++ Sprint 2/src/ver2/Doctor.java
@@ -0,0 +1,41 @@
+package ver2;
+import java.util.ArrayList;
+
+public class Doctor extends Person {
+	
+	private String notes;
+	
+	private ArrayList<Patient> patients;
+	
+	private ArrayList<Receptionist> receptionists;
+	
+	public Doctor(String name) {
+		super.setName(name);
+	}
+	//@OneToMany(mappedBy = "doctor")
+	public ArrayList<Patient> getPatient() {
+		return this.patients;
+	}
+	
+	//@OneToOne(optional = false)
+	public ArrayList<Receptionist> getReceptionist() {
+		return this.receptionists;
+	}
+	
+	public void getPrescriptions() {
+	}
+	
+	public String toString() {
+		return "Doctor: "+ getName();
+	}
+	public void Note (String Notes) {
+		this.setNotes(Notes);
+	}
+	public String getNotes() {
+		return notes;
+	}
+	public void setNotes(String notes) {
+		this.notes = notes;
+	}
+	
+}
