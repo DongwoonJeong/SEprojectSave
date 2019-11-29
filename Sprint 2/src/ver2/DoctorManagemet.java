0741c00df536b5b334ad198ff992ccb8827ac84a
utsavpatel5 <54430022+utsavpatel5@users.noreply.github.com>
2019-11-29 18:17:52 -0500

    updated DoctorManagement class
    fixed some errors and sentence interpretation in paybill and writeNotes method

--- Sprint 2/src/ver2/DoctorManagemet.java
+++ Sprint 2/src/ver2/DoctorManagemet.java
@@ -14,103 +14,106 @@ public class DoctorManagemet {
 
 	private Set<Doctor> doctors;
 
-
+    
 	public static void main(String[] args) throws Exception 
-	  { 
+	{ 
 
-	      writeNotesAsInstructed();
+		writeNotesAsInstructed();
 
-	      chargeFees();
+		chargeFees();
 
 	}
+	//writing notes for patients
 	public static void writeNotesAsInstructed() throws FileNotFoundException{
 		File file = new File("appointmentFile.txt"); 
-				    Scanner scan = new Scanner(file); 
-				  
-				    while (scan.hasNextLine()) 
-				      System.out.println(scan.nextLine()); 
-	       System.out.println();
-	     System.out.println("Please enter the patient name from the AppointmentFile to add notes? ");
-			Scanner input = new Scanner(System.in);
-			System.out.println("Enter a patient name: ");
-			String name = input.next();	
-			
-	            System.out.println("Choice - 1 : Enter the Diagnosis of the Patient");
-	            System.out.println("Choice - 2 : Do you want to write a prescription"); 
-	            
+		Scanner scan = new Scanner(file); 
 
-			int choice = input.nextInt();		
-	      if  (choice == 1){
-	        System.out.printf("Enter the Diagnosis:");
-	       Scanner sc = new Scanner(System.in);
+		while (scan.hasNextLine()) 
+			System.out.println(scan.nextLine()); 
+		System.out.println();
+		System.out.println("Please enter the patient name from the AppointmentFile to add notes? ");
+		Scanner input = new Scanner(System.in);
+		System.out.println("Enter a patient name: ");
+		String name = input.next();	
+
+		System.out.println("Choice - 1 : Enter the Diagnosis of the Patient");
+		System.out.println("Choice - 2 : Do you want to get  a prescription"); 
+
+
+		int choice = input.nextInt();		
+		if  (choice == 1){
+			System.out.printf("Enter the Diagnosis:");
+			Scanner sc = new Scanner(System.in);
 			String reason = sc.nextLine();
 			String textToAppend = "Patient name: " + name + " The Reason: "+ reason;
-	      try{
-	       BufferedWriter writer = new BufferedWriter(
-	       
-		                                new FileWriter("doctorNotes.txt", true)  
-		                            ); 
-	                               	    writer.newLine();   //Add new line
-		    writer.write(textToAppend);
-		    writer.close();
-		   
-	       } catch (IOException e){
-	    	     
-	    	   
-	                               }
-	 
-             System.out.printf("The diagnosis has been saved ");
-	      }	
-	        if  (choice == 2){
-	       System.out.println("What presistion do you want to prescribe for the Patient");
-	       Scanner sc = new Scanner(System.in);
-			System.out.println("Enter reason which needs prescription for: ");
-	      String reason = sc.nextLine();
-	      System.out.println("Patient: " + name + " Has following problem which needs prescibed: " + reason);
-	       String textToAppend = "Patient name: " + name + " The Reason: "+ reason;
-	      try{
-	       BufferedWriter writer = new BufferedWriter(
-	       
-		                                new FileWriter("doctorNotes.txt", true)  
-		                            ); 
-	                               	    writer.newLine();   //Add new line
-		    writer.write(textToAppend);
-		    writer.close();
-	       } catch (IOException e){
-	                               }
+			try{
+				BufferedWriter writer = new BufferedWriter(
 
-	      }
-	       
-	       
-			
+						new FileWriter("doctorNotes.txt", true)  
+						); 
+				writer.newLine();   //Add new line
+				writer.write(textToAppend);
+				writer.close();
+
+			} catch (IOException e){
+
+
+			}
+
+			System.out.printf("The diagnosis has been saved ");
+				}	
+		    System.out.println();
+		System.out.println("***************************************");
+		if  (choice == 2){
+			System.out.println("What prescription do you want to prescribe for the Patient ?");
+			Scanner sc = new Scanner(System.in);
+			System.out.println("Enter reason for which patient needs prescription for ?");
+			String reason = sc.nextLine();
+			System.out.println("Patient: " + name + " Has following problem which needs prescibed: " + reason);
+			String textToAppend = "Patient name: " + name + " The Reason: "+ reason;
+			try{
+				BufferedWriter writer = new BufferedWriter(
+
+						new FileWriter("doctorNotes.txt", true)  
+						); 
+				writer.newLine();   //Add new line
+				writer.write(textToAppend);
+				writer.close();
+			} catch (IOException e){
+			}
+
 		}
 
+
+
+	}
+    //payBill methods can charge fees to patients
 	public static void chargeFees() throws FileNotFoundException{
-		System.out.println("Welcome to Fee Manager");
+		System.out.println("Welcome to patient Billing section");
 		System.out.println("Based on doctorNotes the following charges are presented. ");
 		File file = new File("doctorNotes.txt"); 
-	    Scanner scan = new Scanner(file); 
-	  
-	    while (scan.hasNextLine()) 
-	      System.out.println(scan.nextLine()); 
-	System.out.println();
+		Scanner scan = new Scanner(file); 
+
+		while (scan.hasNextLine()) 
+			System.out.println(scan.nextLine()); 
+		System.out.println();
 		Scanner input = new Scanner(System.in);
-		System.out.println("Enter a patient name to send billing? ");
+		System.out.println("Enter a patient name to send billing information? ");
 		String name = input.next();		
-		System.out.println("The Regular Price is 45$ ");
-		System.out.println("This price is just for doctor visit the treatments will be charges based on symptons");
+		System.out.println("The appointment fee for doctor visit is 45$ ");
+		System.out.println("This price is just for doctor visit, any further fees will be charged  based on symptons");
 		System.out.println("Enter a patient name to send billing? " + name);
 		System.out.println("Option - 1 : Regular Price + Billing for Injuries");
 		System.out.println("Option - 2 : Regualar Price + Billing for Illness");
 		System.out.println("Option - 3 : Regular Price ");
 		System.out.println("Any other Option is not validated and terminated");
-		System.out.println("From Option pick one : ");
-		
+		System.out.println("From the above options please pick one : ");
+
 		int Option = input.nextInt();	
 		if (Option == 3) {
 			System.out.println("The Regular price is charged only");
 			System.out.println(name + " has Bill of $ 45");
-			
+
 		}
 		if (Option == 1) {
 			System.out.println("Please pick injury from the below list");
@@ -123,27 +126,27 @@ public class DoctorManagemet {
 				System.out.println("Shoulder Injury");
 				int num1 = 500, num2 = 45, sum;
 				sum = num1 + num2;
-			System.out.println(name + " has Bill of $ "+ sum);
-			System.out.println("The Regular price: 45$");
-			System.out.println("Injury Cost: 500$");
+				System.out.println(name + " has Bill of $ "+ sum);
+				System.out.println("The Regular price: 45$");
+				System.out.println("Injury Cost: 500$");
 			}
 			if (pick ==2) {
 				System.out.println("Hamstring");
 				int num1 = 600, num2 = 45, sum;
 				sum = num1 + num2;
-			System.out.println(name + " has Bill of $ "+ sum);
-			System.out.println("The Regular price: 45$");
-			System.out.println("Injury Cost: 600$");
+				System.out.println(name + " has Bill of $ "+ sum);
+				System.out.println("The Regular price: 45$");
+				System.out.println("Injury Cost: 600$");
 			} 
 			if (pick ==3) {
 				System.out.println("Groin Pull");
 				int num1 = 700, num2 = 45, sum;
 				sum = num1 + num2;
-			System.out.println(name + " has Bill of $ "+ sum);
-			System.out.println("The Regular price: 45$");
-			System.out.println("Injury Cost: 700$");
+				System.out.println(name + " has Bill of $ "+ sum);
+				System.out.println("The Regular price: 45$");
+				System.out.println("Injury Cost: 700$");
 			}
-			
+
 		}
 		if (Option == 2) {
 			System.out.println("Please pick Illness from the below list");
@@ -157,35 +160,35 @@ public class DoctorManagemet {
 				System.out.println("Flu");
 				int num1 = 100, num2 = 45, sum;
 				sum = num1 + num2;
-			System.out.println(name + " has Bill of $ "+ sum);
-			System.out.println("The Regular price: 45$");
-			System.out.println("Injury Cost: 100$");
+				System.out.println(name + " has Bill of $ "+ sum);
+				System.out.println("The Regular price: 45$");
+				System.out.println("Injury Cost: 100$");
 			}
 			if (pick ==2) {
 				System.out.println("Sore Throat");
 				int num1 = 55, num2 = 45, sum;
 				sum = num1 + num2;
-			System.out.println(name + " has Bill of $ "+ sum);
-			System.out.println("The Regular price: 45$");
-			System.out.println("Injury Cost: 55$");
+				System.out.println(name + " has Bill of $ "+ sum);
+				System.out.println("The Regular price: 45$");
+				System.out.println("Injury Cost: 55$");
 			} 
 			if (pick ==3) {
 				System.out.println("Cough");
 				int num1 = 85, num2 = 45, sum;
 				sum = num1 + num2;
-			System.out.println(name + " has Bill of $ "+ sum);
-			System.out.println("The Regular price: 45$");
-			System.out.println("Injury Cost: 85$");
+				System.out.println(name + " has Bill of $ "+ sum);
+				System.out.println("The Regular price: 45$");
+				System.out.println("Injury Cost: 85$");
 			}
 			if (pick ==4) {
 				System.out.println("Ear Pain");
 				int num1 = 155, num2 = 45, sum;
 				sum = num1 + num2;
-			System.out.println(name + " has Bill of $ "+ sum);
-			System.out.println("The Regular price: 45$");
-			System.out.println("Injury Cost: 155$");
+				System.out.println(name + " has Bill of $ "+ sum);
+				System.out.println("The Regular price: 45$");
+				System.out.println("Injury Cost: 155$");
 			}
-			
+
 		}
 	}
 
