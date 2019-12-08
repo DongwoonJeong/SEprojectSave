1184aa5d44b7037ff053324b179fbea1bdc1bfc0
Dongwoon Jeong <rjrd1950@gmail.com>
2019-12-08 17:17:03 -0500

    getPerscription has problem in doctor class.

--- Sprint 2/src/ver2/Doctor.java
+++ Sprint 2/src/ver2/Doctor.java
@@ -1,6 +1,9 @@
 package ver2;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+/*
+ * package ver2;
+import java.util.ArrayList;
 
 public class Doctor extends Person {
 	
@@ -23,11 +26,51 @@ public class Doctor extends Person {
 		return this.receptionists;
 	}
 	
-	public void getPrescriptions()throws FileNotFoundException {
-		
-		HospitalManagement HospitalManagementObject = new HospitalManagement();
-		HospitalManagementObject.getPatientPrescriptions();
+	public void getPrescriptions() {
 	}
+	
+	public String toString() {
+		return "Doctor: "+ getName();
+	}
+	public void Note (String Notes) {
+		this.setNotes(Notes);
+	}
+	public String getNotes() {
+		return notes;
+	}
+	public void setNotes(String notes) {
+		this.notes = notes;
+	}
+	
+}
+ * 
+ * */
+public class Doctor extends Person {
+	
+	private String notes;
+	
+	private ArrayList<Patient> patients;
+	
+	private ArrayList<Receptionist> receptionists;
+	
+	public Doctor(String name) {
+		super.setName(name);
+	}
+	//@OneToMany(mappedBy = "doctor")
+	public ArrayList<Patient> getPatient() {
+		return this.patients;
+	}
+	
+	//@OneToOne(optional = false)
+	public ArrayList<Receptionist> getReceptionist() {
+		return this.receptionists;
+	}
+	
+	/*public void getPrescriptions()throws FileNotFoundException {
+		
+		DoctorManagemet DoctorManagemetObject = new DoctorManagemet();
+		DoctorManagemetObject.chargeFees();
+	}*/
 	
 	public String toString() {
 		return "Doctor: "+ getName();
